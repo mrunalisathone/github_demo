f17f9a07b5161354d3b7e93970d265fc115b6119
mrunali <mrunalianilsathone21@gmail.com>
2017-11-07 10:00:37 +0530

    changed

--- src/pallindrome_number.py
+++ src/pallindrome_number.py
@@ -1,4 +1,11 @@
 # TODO: Find all one digit to 10 digit numbers for which abcd * 4 = dcba
 
 def find_all_pallindrome_numbers(number):
-    pass
+    for number in range(1000, 10000):
+        rev = int(str(number)[::-1])
+        if number * 4 == rev:
+            return number
+
+
+x = find_all_pallindrome_numbers()
+print(x)
